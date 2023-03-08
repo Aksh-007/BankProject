f9293ff4d8080cc607d80a3ed84649b86cebbacd
Aksh-007 <akshaybarapatre54@gmail.com>
2023-03-09 00:17:02 +0530

    created constructor of class

---
+++ bank.js
@@ -0,0 +1,14 @@
+
+class Bank {
+
+    // created constructor of class Bank
+    // with default paramater =0
+    constructor(balance = 0, name){
+        this.balance = balance
+        this.name = name
+    }
+    
+
+}
+
+
